0f0d4f16e15b1ad3f5805ae96092d6bdc10d564e
dickson4954 <dicksonmurithi155@gmail.com>
2024-04-30 08:32:16 +0300

    initial commit

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
     return (
-        <div className=" mx-auto m-10 p-10 mx-auto btn btn-warning">
+        <div className="mx-auto btn btn-warning">
       <header className = {"Header"}>
         <h1>The Royal Bank of Flatiron</h1>
       </header>
